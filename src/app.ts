769bafefd63f1affba65ef612395d2c505d65ddb
Joera <joeramulders@gmail.com>
2021-05-30 14:43:04 +0200

    hopelijk niks

--- src/app.ts
+++ src/app.ts
@@ -111,13 +111,7 @@ document.getElementById('publish')?.addEventListener('click', () => {
   const type = (<HTMLInputElement>document.getElementById('publicationType'))?.value
   console.log('type', type)
   // TODO build JSON-LD based on the schema of the selected type
-})
 
-// function registerOnENS async (ens_domain: string, identifyer: string) {
-//         const ethProvider = await web3Modal.connect()
-//         const ens = new ENS({ethProvider, ensAddress: getEnsAddress('1')})
-//         const ENSName = ens.name(ens_domain)
-//         const subdomain_tx = await ENSName.createSubdomain(identifyer)
-//
-//         return true;
-//     }
+
+
+})
